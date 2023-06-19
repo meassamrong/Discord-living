56156d98f6efea7777e1569f4841e3e60b15179d
meassamrong <meassamrong99@gmail.com>
2023-06-19 09:16:41 +0700

    update

--- RichPresence.js
+++ RichPresence.js
@@ -2,23 +2,23 @@ const Discord = require('discord.js-selfbot-v13');
 const RichPresence = {
     setRichPresec(client) {
         const r = new Discord.RichPresence()
-            .setApplicationId('891905157409931294')
+            .setApplicationId('817229550684471297')
             .setType('STREAMING')
             .setURL('https://youtube.com/watch?v=dQw4w9WgXcQ')
-            .setState('OnlyFans')
-            .setName("J'CORP")
-            .setDetails('Everybody happy to watch this')
+            .setState('A lot')
+            .setName('Watching')
+            .setDetails('OnlyFans')
             .setParty({
                 max: 99,
                 current: 90,
                 id: Discord.getUUID(),
             })
             .setStartTimestamp(Date.now())
-            .setAssetsLargeImage('929325841350000660')
-            .setAssetsLargeText('Audience')
+            .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1010141489604599850/1120173852069216356/929325841350000660.jpg')
+            .setAssetsLargeText('OnlyFans')
             .setAssetsSmallImage('895316294222635008')
-            .setAssetsSmallText('18+ school')
-            .addButton('Join', 'https://link.com/')
+            .setAssetsSmallText('18+')
+            .addButton('Join', 'https://youtu.be/vbcHDizVn9I')
         client.user.setActivity(r);
     }
 }
